66e49a8eb21b49d8da4ef90e752129a3860e7e0b
ljf <1062134582@qq.com>
2019-05-19 21:47:37 +0800

    完善游戏界面逻辑

--- gobangClient/src/page/main.vue
+++ gobangClient/src/page/main.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="main">
-    <h1 class="title">--益智五子棋--</h1>
+    <h1 class="title">{{ title }}</h1>
     <canvas ref="chess" width="500px" height="500px" @click="play"></canvas>
     <div class="footer">
-      <el-button type="primary" round>重新开始</el-button>
+      <el-button type="primary" round @click="reload">重新开始</el-button>
     </div>
   </div>
 </template>
@@ -12,10 +12,18 @@
   export default {
     data() {
       return {
-        'context': null,  // 画布
-        'ws': null,       // websocket
-        'over': false,    // 是否结束
-        'me': true,       // 是否到我
+        'title': '--益智五子棋--',
+        'context': null,                        // 画布
+        'url': 'ws://127.0.0.1:5006/game/001',  // websocket地址
+        'ws': null,                             // websocket对象
+        'over': false,                          // 是否结束
+        'me': true,                             // 是否到我
+        'curI': 0,                              // 记录计算机当前下棋的i坐标
+        'curJ': 0,                              // 记录计算机当前下棋的j坐标
+        'myWin': [],                           // 记录我赢的情况
+        'compWin': [],                         // 记录计算机赢的情况
+        'myWinArr': [],                            // 我赢的统计数组
+        'computerWinArr': [],                      // 计算机赢的统计数组
       }
     },
     created() {
@@ -25,9 +33,17 @@
         this.context.strokeStyle = '#bfbfbf'; //边框颜色
         this.drawChessBoard(); // 画棋盘
       });
-      this.initWebSocket()
+      this.initWebSocket();
+
+      for (let i = 0; i < this.total; i++) {
+        this.myWinArr[i] = 0;
+        this.myWin[i] = 0;
+        this.computerWinArr[i] = 0;
+        this.compWin[i] = 0;
+      }
     },
     computed: {
+      // 棋盘数组
       chressBord() {
         let chressList = [];
         for (let i = 0; i < 15; i++) {
@@ -38,55 +54,66 @@
         }
         return chressList;
       },
-      count() {
-        let count = 0;
+      // 赢法数组
+      wins() {
+        let wins = [];
+        for (let i = 0; i < 15; i++) {
+          wins[i] = [];
+          for (let j = 0; j < 15; j++) {
+            wins[i][j] = [];
+          }
+        }
+        return wins;
+      },
+      // 赢法总数
+      total() {
+        let total = 0;
         //横线赢法
-        for (var i = 0; i < 15; i++) {
-          for (var j = 0; j < 11; j++) {
-            for (var k = 0; k < 5; k++) {
-              wins[i][j + k][count] = true;
+        for (let i = 0; i < 15; i++) {
+          for (let j = 0; j < 11; j++) {
+            for (let k = 0; k < 5; k++) {
+              this.wins[i][j + k][total] = true;
             }
-            count++;
+            total++;
           }
         }
 
         //竖线赢法
-        for (var i = 0; i < 15; i++) {
-          for (var j = 0; j < 11; j++) {
-            for (var k = 0; k < 5; k++) {
-              wins[j + k][i][count] = true;
+        for (let i = 0; i < 15; i++) {
+          for (let j = 0; j < 11; j++) {
+            for (let k = 0; k < 5; k++) {
+              this.wins[j + k][i][total] = true;
             }
-            count++;
+            total++;
           }
         }
 
         //正斜线赢法
-        for (var i = 0; i < 11; i++) {
-          for (var j = 0; j < 11; j++) {
-            for (var k = 0; k < 5; k++) {
-              wins[i + k][j + k][count] = true;
+        for (let i = 0; i < 11; i++) {
+          for (let j = 0; j < 11; j++) {
+            for (let k = 0; k < 5; k++) {
+              this.wins[i + k][j + k][total] = true;
             }
-            count++;
+            total++;
           }
         }
 
         //反斜线赢法
-        for (var i = 0; i < 11; i++) {
-          for (var j = 14; j > 3; j--) {
-            for (var k = 0; k < 5; k++) {
-              wins[i + k][j - k][count] = true;
+        for (let i = 0; i < 11; i++) {
+          for (let j = 14; j > 3; j--) {
+            for (let k = 0; k < 5; k++) {
+              this.wins[i + k][j - k][total] = true;
             }
-            count++;
+            total++;
           }
         }
 
-        return count;
+        return total;
       }
     },
     methods: {
       initWebSocket() {
-        let url = 'ws://127.0.0.1:5006/game/001';
-        this.ws = new WebSocket(url);
+        this.ws = new WebSocket(this.url);
         this.ws.onopen = this.webSocketOnOpen;
         this.ws.onerror = this.webSocketOnError;
         this.ws.onmessage = this.webSocketOnMessage;
@@ -116,46 +143,127 @@
         let i = data['x'];
         let j = data['y'];
         let chair = data['chair'];
+        console.log(this.total);
         if (!this.chressBord[i][j]) {
           this.oneStep(i, j, this.me);
           this.chressBord[i][j] = chair; //我，已占位置
-          console.log(this.count);
-          for (let k = 0; k < this.count; k++) { // 将可能赢的情况都加1
-            if (wins[i][j][k]) {
-              myWin[k]++;
-              _compWin[k] = computerWin[k];
-              computerWin[k] = 6;//这个位置对方不可能赢了
-              if (myWin[k] == 5) {
-                resultTxt.innerHTML = '恭喜，你赢了！';
+          for (let k = 0; k < this.total; k++) { // 将可能赢的情况都加1
+            if (this.wins[i][j][k]) {
+              this.myWinArr[k]++;
+              this.compWin[k] = this.computerWinArr[k];
+              this.computerWinArr[k] = 6;  // 这个位置对方不可能赢了
+              if (this.myWinArr[k] == 5) {
+                this.title = '恭喜，你赢了！';
                 this.over = true;
               }
             }
           }
           if (!this.over) {
             this.me = !this.me;
-            computerAI();
+            this.computerAI();
           }
         }
       },
-      doPlayChessRobot() {
-      },
-      oneStep(i, j, me) {
-        let context = this.context;
-        context.beginPath();
-        context.arc(40 + i * 30, 40 + j * 30, 13, 0, 2 * Math.PI);// 画圆
-        context.closePath();
-        //渐变
-        let gradient = context.createRadialGradient(40 + i * 30 + 2, 40 + j * 30 - 2, 13, 40 + i * 30 + 2, 40 + j * 30 - 2, 0);
+      doPlayChessRobot(data) {
+        let i = data['x']
+        let j = data['y']
+        let chair = data['chair']
+        if (this.chressBord[i][j] == 0) {
+          this.oneStep(i, j, false);
+          this.chressBord[i][j] = chair;    // 机器人占据位置
 
-        if (me) {
-          gradient.addColorStop(0, '#0a0a0a');
-          gradient.addColorStop(1, '#636766');
-        } else {
-          gradient.addColorStop(0, '#d1d1d1');
-          gradient.addColorStop(1, '#f9f9f9');
+          for (let k = 0; k < this.total; k++) {
+            if (this.wins[i][j][k]) {
+              this.computerWinArr[k]++;
+              this.myWin[k] = this.myWinArr[k];
+              this.myWinArr[k] = 6;    // 这个位置对方不可能赢了
+              if (this.computerWinArr[k] == 5) {
+                this.title = 'o(╯□╰)o，计算机赢了，继续加油哦！';
+                this.over = true;
+              }
+            }
+          }
+
+          if (!this.over) {
+            this.me = !this.me;
+          }
         }
-        context.fillStyle = gradient;
-        context.fill();
+      },
+      computerAI() {
+        let myScore = [];
+        let computerScore = [];
+        let max = 0;
+        let u = 0, v = 0;
+        for (let i = 0; i < 15; i++) {
+          myScore[i] = [];
+          computerScore[i] = [];
+          for (let j = 0; j < 15; j++) {
+            myScore[i][j] = 0;
+            computerScore[i][j] = 0;
+          }
+        }
+        for (let i = 0; i < 15; i++) {
+          for (let j = 0; j < 15; j++) {
+            if (this.chressBord[i][j] == 0) {
+              for (let k = 0; k < this.total; k++) {
+                if (this.wins[i][j][k]) {
+                  if (this.myWinArr[k] == 1) {
+                    myScore[i][j] += 200;
+                  } else if (this.myWinArr[k] == 2) {
+                    myScore[i][j] += 400;
+                  } else if (this.myWinArr[k] == 3) {
+                    myScore[i][j] += 2000;
+                  } else if (this.myWinArr[k] == 4) {
+                    myScore[i][j] += 10000;
+                  }
+
+                  if (this.computerWinArr[k] == 1) {
+                    computerScore[i][j] += 220;
+                  } else if (this.computerWinArr[k] == 2) {
+                    computerScore[i][j] += 420;
+                  } else if (this.computerWinArr[k] == 3) {
+                    computerScore[i][j] += 2100;
+                  } else if (this.computerWinArr[k] == 4) {
+                    computerScore[i][j] += 20000;
+                  }
+                }
+              }
+
+              if (myScore[i][j] > max) {
+                max = myScore[i][j];
+                u = i;
+                v = j;
+              } else if (myScore[i][j] == max) {
+                if (computerScore[i][j] > computerScore[u][v]) {
+                  u = i;
+                  v = j;
+                }
+              }
+
+              if (computerScore[i][j] > max) {
+                max = computerScore[i][j];
+                u = i;
+                v = j;
+              } else if (computerScore[i][j] == max) {
+                if (myScore[i][j] > myScore[u][v]) {
+                  u = i;
+                  v = j;
+                }
+              }
+
+            }
+          }
+        }
+
+        this.curI = u;
+        this.curJ = v;
+
+        this.sendMsg({
+          'x': u,
+          'y': v,
+          'chair': 2,
+          'action': 'Robot_Play_Chess',
+        });
       },
       drawChessBoard() {
         for (let i = 0; i < 15; i++) {
@@ -175,8 +283,10 @@
 
         let x = e.offsetX;
         let y = e.offsetY;
-        let i = Math.floor(x / 30);
-        let j = Math.floor(y / 30);
+        let i = Math.floor(x / 30) - 1;
+        let j = Math.floor(y / 30) - 1;
+
+        console.log(x, y, i, j);
 
         if (!this.chressBord[i][j]) {
           this.sendMsg({
@@ -186,6 +296,29 @@
             'action': 'Player_Play_Chess'
           });
         }
+      },
+      reload() {
+        this.$nextTick(() => {
+          window.location.reload();
+        });
+      },
+      oneStep(i, j, me) {
+        let context = this.context;
+        context.beginPath();
+        context.arc(40 + i * 30, 40 + j * 30, 13, 0, 2 * Math.PI);// 画圆
+        context.closePath();
+        //渐变
+        let gradient = context.createRadialGradient(40 + i * 30 + 2, 40 + j * 30 - 2, 13, 40 + i * 30 + 2, 40 + j * 30 - 2, 0);
+
+        if (me) {
+          gradient.addColorStop(0, '#0a0a0a');
+          gradient.addColorStop(1, '#636766');
+        } else {
+          gradient.addColorStop(0, '#d1d1d1');
+          gradient.addColorStop(1, '#f9f9f9');
+        }
+        context.fillStyle = gradient;
+        context.fill();
       }
     }
   }
